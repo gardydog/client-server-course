a219b1efe5562c220b419acfb30cf743ed7d75d6
Alexander <gardydog@yandex.ru>
2021-06-28 21:45:28 +0300

    1. Спроектированы классы User, Photo, Group для представления соответствующих данных приложения; 2. Изменены методы получения данных от VK API. Преобразованы json-ответы сервера в спроектированные классы. 3. На ранее созданных экранах выполнены необходимые запросы к API и отображены полученные данные.

---
+++ VKApp2-3(1)/VKApp1/Groups/GroupsTableViewCell.swift
@@ -0,0 +1,31 @@
+//
+//  GroupsTableViewCell.swift
+//  VKApp1
+//
+//  Created by Mac on 22.05.2021.
+//
+
+import UIKit
+
+class GroupsTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var groupImage: UIImageView!
+    @IBOutlet weak var groupNameLabel: UILabel!
+    
+    func clearCell() {
+        groupImage.image = nil
+        groupNameLabel.text = nil
+    }
+    
+    override func prepareForReuse() {
+        clearCell()
+    }
+    
+    func configure(group: GroupsItemsCodable) {
+        groupNameLabel.text = group.name
+        groupImage.sd_setImage(with: URL(string: group.avatar), placeholderImage: UIImage(named: "glasses"))
+    }
+    
+    
+    
+}
